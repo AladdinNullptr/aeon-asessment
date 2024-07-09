91ce50e19a3d2434309f5e1aeed9ecc0a860f3bd
AladdinNullptr <83447228+AladdinNullptr@users.noreply.github.com>
2024-07-09 19:31:43 +0800

    two sum map rename

--- app/question3/twoSum.ts
+++ app/question3/twoSum.ts
@@ -1,11 +1,11 @@
 const twoSum = function (nums: number[], target: number): number[] {
-  const hash = new Map();
+  const map = new Map();
 
   for (let i = 0; i < nums.length; i++) {
     const num = nums[i];
     const remainder = target - num;
 
-    const remainderIndex = hash.get(remainder);
+    const remainderIndex = map.get(remainder);
 
     // ignore duplicate numbers. Only use the first occurrence
     // to cover cases like [0, 1, 2, 2, 3, 5]
@@ -13,7 +13,7 @@ const twoSum = function (nums: number[], target: number): number[] {
 
     if (remainderIndex !== undefined) return [remainderIndex + 1, i + 1];
 
-    hash.set(num, i);
+    map.set(num, i);
   }
 
   return [];
